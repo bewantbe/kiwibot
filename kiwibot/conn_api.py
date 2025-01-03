4ec37eea5db7f0431d95bf3c28238aabae645f13
Eddy Xiao <bewantbe@gmail.com>
2025-01-03 23:30:58 +0800

    move code.

--- kiwibot/conn_api.py
+++ kiwibot/conn_api.py
@@ -333,3 +333,51 @@ class FeishuPortal:
         thread = threading.Thread(target=send_from_queue)
         thread.daemon = True
         thread.start()
+
+def old_main():
+    load_dotenv()
+
+    webhook_url = os.getenv('GROUP_WEBHOOK_URL')
+    webhook_secret = os.getenv('GROUP_WEBHOOK_SECRET')
+
+    # Test send message to group
+    if 0:
+        group_bot_send_msg("text", "Hello, Kiwi!", webhook_url, webhook_secret)
+
+    app_id = os.getenv('APP_ID')
+    app_secret = os.getenv('APP_SECRET')
+
+    client = lark.Client.builder() \
+        .app_id(app_id) \
+        .app_secret(app_secret) \
+        .log_level(lark.LogLevel.DEBUG) \
+        .build()
+
+    if 0:
+        # Upload image
+        img_path = 'test_bot.webp'
+
+        r = upload_img(img_path, client)
+        print(r)
+        image_key = r['image_key']
+        print(image_key)
+    else:
+        image_key = "img_v3_02i2_45d27df1-9978-447f-bd35-a43a7b844c4g"
+
+    if 0:
+        # Send message with image
+        content = {
+            "image_key": image_key
+        }
+        group_bot_send_msg("image", content, webhook_url, webhook_secret)
+    
+    if 0:
+        # test application bot
+        user_id_in_app = 'ou_0b2ada4556de2f7b7ddc6557b6f4292b'
+        send_msg_to_user(user_id_in_app, "text", "Hello, this is Kiwi!", client)
+
+    start_echo_bot(client)
+
+if __name__ == '__main__':
+    #old_main()
+    pass
